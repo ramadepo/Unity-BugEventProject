fd16803a45755366017f3def1cc276a4f65278dc
ramadepo <iammastercloud@gmail.com>
2018-04-13 23:57:08 +0800

    add RR.asset for post processing

--- Assets/Scripts/SpiderManage.cs
+++ Assets/Scripts/SpiderManage.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class SpiderManage : MonoBehaviour {
 
+	public PostProcessingBehaviour camEffects;
+	public PostProcessingProfile CC;
+	public PostProcessingProfile RR;
 	public int bugCount = 0;
+
+
+
+	private void Update(){
+		if (bugCount == 0) {
+			camEffects.profile = CC;
+		} else {
+			camEffects.profile = RR;
+		}
+	}
 
 	public void AddBug(){
 		bugCount++;
