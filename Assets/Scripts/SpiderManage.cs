844ab428c31095029c56a4e0a556b3d4140561d1
ramadepo <iammastercloud@gmail.com>
2018-04-14 12:52:50 +0800

    red vignette complete

--- Assets/Scripts/SpiderManage.cs
+++ Assets/Scripts/SpiderManage.cs
@@ -5,20 +5,32 @@ using UnityEngine.PostProcessing;
 
 public class SpiderManage : MonoBehaviour {
 
-	public PostProcessingBehaviour camEffects;
 	public PostProcessingProfile CC;
-	public PostProcessingProfile RR;
-	public int bugCount = 0;
+	private VignetteModel.Settings vvset;
+	private float redTime;
+	private bool reding;
 
-
+	private void Start(){
+		redTime = -100f;
+		reding = false;
+	}
 
 	private void Update(){
-		if (bugCount == 0) {
-			camEffects.profile = CC;
+		vvset = CC.vignette.settings;
+		if (bugCount > 0) {
+			if (Time.time-redTime >= 4.5f) {	//count for when reder and when blacker
+				reding = true;
+				redTime = Time.time;
+			}
+		}
+		if (reding) {
+			Reder ();
 		} else {
-			camEffects.profile = RR;
+			Blacker ();
 		}
 	}
+
+	public int bugCount = 0;
 
 	public void AddBug(){
 		bugCount++;
@@ -26,6 +38,31 @@ public class SpiderManage : MonoBehaviour {
 
 	public void KillBug(){
 		bugCount--;
+		if (bugCount == 0) {	//reset vignette
+			vvset.color.r = 0f;
+			reding = false;
+			CC.vignette.settings = vvset;
+		}
+	}
+
+	private void Reder(){	//red vignette
+		if (vvset.color.r < 1f) {
+			vvset.color.r += 0.3922f * Time.deltaTime;
+		}
+		else {
+			vvset.color.r = 1f;
+			reding = false;
+		}
+		CC.vignette.settings = vvset;
+	}
+
+	private void Blacker(){	//return normal vignette
+		if (vvset.color.r > 0f) {
+			vvset.color.r -= 0.3922f * Time.deltaTime;
+		} else {
+			vvset.color.r = 0f;
+		}
+		CC.vignette.settings = vvset;
 	}
 
 }
