e8d0be9947088a578a2466aceae9b49a7ff03c6d
ramadepo <iammastercloud@gmail.com>
2018-05-21 23:41:57 +0800

    temp

--- Assets/Scripts/WomanWalkAround.cs
+++ Assets/Scripts/WomanWalkAround.cs
@@ -10,14 +10,22 @@ public class WomanWalkAround : MonoBehaviour {
 	private NavMeshAgent nav;
 	public float time1 = 20f;
 	public float time2 = 40f;
+	public Animator ani;
 
 	// Use this for initialization
 	void Start () {
 		nav = GetComponent<NavMeshAgent> ();
 		Invoke ("Walk", Random.Range (10f, 20f));
 	}
-	
+
+	void Update(){
+		if (Vector3.Distance(transform.position,WalkToPlace.transform.GetChild(PlaceNum).position) <= 1.5f) {
+			ani.SetBool ("IsWalking", false);
+		}
+	}
+
 	private void Walk(){
+		ani.SetBool ("IsWalking", true);
 		PlaceNum = Random.Range (0, WalkToPlace.transform.childCount);
 		nav.SetDestination (WalkToPlace.transform.GetChild (PlaceNum).position);
 		Invoke ("Walk", Random.Range (time1, time2));
